5d2872c2674d587e9a2d7df1f23cf66cce01e49d
vinaydokka <vinay.dokka@nexgdata.com>
2020-02-27 17:43:02 +0530

    default parametersconcepts

---
+++ latest-practice/artemeticops.js
@@ -0,0 +1,79 @@
+/* let a = 2;
+let b = 3;
+let sum = a ** b;
+console.log(sum); */
+/* 
+
+let num1 = 20;
+let num2 = 30;
+let sum = num1 + num2;
+//sum = sum + 20;
+//sum = sum - 20;
+sum -= 20;
+//sum += 20;
+console.log(sum); */
+
+/* 
+let a = '10';
+let b = 10;
+if (a === b) {
+    console.log('is equal value')
+} else {
+    console.log('is not equal ')
+} */
+
+
+
+/* let a = '100';
+let b = 10;
+if (a == b) {
+    console.log('is equal value')
+} else {
+    console.log('is not equal ')
+} */
+
+/* let a = 10;
+let b = 10;
+if (a != b) {
+    console.log('is equal value')
+} else {
+    console.log('is not equal ')
+} */
+
+/* 
+let a = '100';
+let b = 10;
+if (a !== b) {
+    console.log('is equal value')
+} else {
+    console.log('is not equal ')
+} */
+
+
+
+/* let mans = 20;
+let womens = 20;
+let girls = '60';
+let boys = 60;
+let sum = '';
+//console.log(mans == womens);
+if (mans == womens && girls === boys) {
+    console.log(true)
+
+} else {
+    console.log(false);
+
+}
+sum += 20;
+mans = 50;
+womens = 50;
+console.log(mans == womens);
+console.log(sum);
+console.log(`the adding extra ${mans}`) */
+
+
+
+
+let a = "my age is" + 21;
+let b = 'my age is ' + 25;
+console.log(a);
